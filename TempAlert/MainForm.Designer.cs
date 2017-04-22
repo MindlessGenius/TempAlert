e2c082e1df6e5917722f0c3f1ca84716d8f5b2df
Robert Phipps <rphippsy@gmail.com>
2017-04-22 11:56:56 +0100

    Initial commit from thinkpad

---
+++ TempAlert/MainForm.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace TempAlert
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.sensorReadingsTextBox = new System.Windows.Forms.TextBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.maxTempLabel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.appToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.getSupportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showCurrentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reloadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.monitoringToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.snoozeAlertsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.untilRebootToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.sensorReadingsTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 116);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(242, 138);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Individual readings";
+            // 
+            // sensorReadingsTextBox
+            // 
+            this.sensorReadingsTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.sensorReadingsTextBox.Location = new System.Drawing.Point(6, 19);
+            this.sensorReadingsTextBox.Multiline = true;
+            this.sensorReadingsTextBox.Name = "sensorReadingsTextBox";
+            this.sensorReadingsTextBox.ReadOnly = true;
+            this.sensorReadingsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.sensorReadingsTextBox.Size = new System.Drawing.Size(230, 113);
+            this.sensorReadingsTextBox.TabIndex = 0;
+            this.sensorReadingsTextBox.Text = "Loading. Please wait.";
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "TempAlert Client";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 264);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(266, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(249, 17);
+            this.toolStripStatusLabel1.Text = "© Robert Phipps 2017";
+            // 
+            // maxTempLabel
+            // 
+            this.maxTempLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maxTempLabel.Location = new System.Drawing.Point(16, 49);
+            this.maxTempLabel.Name = "maxTempLabel";
+            this.maxTempLabel.Size = new System.Drawing.Size(238, 64);
+            this.maxTempLabel.TabIndex = 2;
+            this.maxTempLabel.Text = "-";
+            this.maxTempLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.appToolStripMenuItem,
+            this.configToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(266, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // appToolStripMenuItem
+            // 
+            this.appToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.snoozeAlertsToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.appToolStripMenuItem.Name = "appToolStripMenuItem";
+            this.appToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.appToolStripMenuItem.Text = "App";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.getSupportToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // configToolStripMenuItem
+            // 
+            this.configToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showCurrentToolStripMenuItem,
+            this.monitoringToolStripMenuItem,
+            this.reloadToolStripMenuItem});
+            this.configToolStripMenuItem.Name = "configToolStripMenuItem";
+            this.configToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.configToolStripMenuItem.Text = "Config";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // getSupportToolStripMenuItem
+            // 
+            this.getSupportToolStripMenuItem.Name = "getSupportToolStripMenuItem";
+            this.getSupportToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.getSupportToolStripMenuItem.Text = "Get support";
+            // 
+            // showCurrentToolStripMenuItem
+            // 
+            this.showCurrentToolStripMenuItem.Name = "showCurrentToolStripMenuItem";
+            this.showCurrentToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.showCurrentToolStripMenuItem.Text = "Show current config";
+            // 
+            // reloadToolStripMenuItem
+            // 
+            this.reloadToolStripMenuItem.Name = "reloadToolStripMenuItem";
+            this.reloadToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.reloadToolStripMenuItem.Text = "Reload config";
+            // 
+            // monitoringToolStripMenuItem
+            // 
+            this.monitoringToolStripMenuItem.Name = "monitoringToolStripMenuItem";
+            this.monitoringToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.monitoringToolStripMenuItem.Text = "Monitoring config";
+            // 
+            // snoozeAlertsToolStripMenuItem
+            // 
+            this.snoozeAlertsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.minsToolStripMenuItem,
+            this.hourToolStripMenuItem,
+            this.untilRebootToolStripMenuItem});
+            this.snoozeAlertsToolStripMenuItem.Name = "snoozeAlertsToolStripMenuItem";
+            this.snoozeAlertsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.snoozeAlertsToolStripMenuItem.Text = "Snooze alerts";
+            // 
+            // minsToolStripMenuItem
+            // 
+            this.minsToolStripMenuItem.Name = "minsToolStripMenuItem";
+            this.minsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.minsToolStripMenuItem.Text = "10 mins";
+            // 
+            // hourToolStripMenuItem
+            // 
+            this.hourToolStripMenuItem.Name = "hourToolStripMenuItem";
+            this.hourToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.hourToolStripMenuItem.Text = "1 hour";
+            // 
+            // untilRebootToolStripMenuItem
+            // 
+            this.untilRebootToolStripMenuItem.Name = "untilRebootToolStripMenuItem";
+            this.untilRebootToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.untilRebootToolStripMenuItem.Text = "Until reboot";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // updateTimer
+            // 
+            this.updateTimer.Interval = 5000;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Current maximum:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 286);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maxTempLabel);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "TempAlert Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.TextBox sensorReadingsTextBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label maxTempLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem appToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem snoozeAlertsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem minsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hourToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem untilRebootToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showCurrentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem monitoringToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reloadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem getSupportToolStripMenuItem;
+        private System.Windows.Forms.Timer updateTimer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TempAlert/MainForm.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenHardwareMonitor.Hardware;
+
+namespace TempAlert
+{
+    public partial class MainForm : Form
+    {
+        private Computer computer = new Computer();
+        private float maxTempThreshold = 50;
+        private float currentMaxTemp = 0;
+        private DateTime nextAlert = DateTime.UtcNow;
+        private bool alerting = true;
+        public MainForm()
+        {
+            InitializeComponent();
+            this.FormClosing += MainForm_FormClosing;
+
+            computer.CPUEnabled = true;
+            computer.GPUEnabled = true;
+            computer.MainboardEnabled = true;
+            computer.HDDEnabled = true;
+            computer.RAMEnabled = true;
+            computer.Open();
+
+            UpdateTemps();
+
+            updateTimer.Tick += UpdateTemps_Tick;
+            updateTimer.Enabled = true;
+        }
+
+        private void UpdateTemps_Tick(object sender, EventArgs e)
+        {
+            UpdateTemps();
+            HighTemperatureAlertCheck();
+        }
+
+        private void UpdateTemps()
+        {
+            var readingsText = new StringBuilder();
+            var tempList = new List<ISensor>();
+            foreach (var hardware in computer.Hardware)
+            {
+                hardware.Update();
+                var tempsensors = hardware.Sensors.Where(s => s.SensorType == SensorType.Temperature);
+                if (tempsensors.Count() == 0)
+                {
+                    // nothing to show for this device
+                    continue;
+                }
+                
+                readingsText.AppendLine(hardware.Name + ":");
+                foreach (var sensor in tempsensors)
+                {
+                    if (sensor.Value.HasValue)
+                    {
+                        tempList.Add(sensor);
+                        readingsText.AppendLine(sensor.Name + " is @ " + sensor.Value + "°C");
+                    }
+                }
+                readingsText.AppendLine("-----");
+            }
+            sensorReadingsTextBox.Text = readingsText.ToString();
+
+            if (tempList.Count > 0)
+            {
+                currentMaxTemp = tempList.Max(t => t.Value).Value;
+                maxTempLabel.Text = currentMaxTemp.ToString() + "°C";
+            } else
+            {
+                maxTempLabel.ResetText();
+            }
+        }
+
+        private void HighTemperatureAlertCheck()
+        {
+            if (nextAlert > DateTime.UtcNow || !alerting)
+            {
+                return;
+            }
+
+            if (currentMaxTemp > maxTempThreshold)
+            {
+                HighTemperatureAlert(currentMaxTemp);
+            }
+        }
+
+        private void HighTemperatureAlert(float max)
+        {
+            alerting = false;
+            MessageBox.Show("A device is reporting abnormally high temperatures. It is reporting a temperature of " + max.ToString() + "°C", "TempAlert Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            nextAlert = DateTime.UtcNow.AddMinutes(5);
+            alerting = true;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                this.Hide();
+                e.Cancel = true;
+                notifyIcon.ShowBalloonTip(10000, "TempAlert is still running", "TempAlert is continuing to monitor your computer in the background.", ToolTipIcon.Info);
+            }
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to quit? This will stop monitoring.", "Quit?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void MainForm_FormClosing(object sender, EventArgs e)
+        {
+            notifyIcon.Dispose();
+        }
+    }
+}
